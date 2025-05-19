386940ea682cb976b676104daf0ff54e2dafeca3
lewisrai <144852757+lewisrai@users.noreply.github.com>
2025-05-19 19:05:41 +0100

    Basic Setup

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+alias yay="yay --aur"
+
+if uwsm check may-start
+	exec uwsm start hyprland.desktop
+end
---
+++ install/install.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+if not fish_is_root_user
+	echo Run this script as root!
+	exit
+end
+
+echo Remove plymouth from /etc/mkinitcpio.conf!
+read
+
+pacman -Syu
+pacman -Rsn plymouth cachyos-plymouth-theme
+pacman -S asusctl hyprland librewolf nvim yay
+cp ../nvidia/blacklist-nouveau.conf /etc/modprobe.d
+cp ../nvidia/00-remove-nvidia.rules /etc/udev/rules.d
+
+yay -Syu
