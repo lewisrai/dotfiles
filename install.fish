5f287e91d2e125298364898a0ca6e2c2a911d7a0
lewisrai <144852757+lewisrai@users.noreply.github.com>
2025-05-30 09:09:34 +0000

    Use sudo per line

--- install.fish
+++ install.fish
@@ -1,31 +1,31 @@
 #!/usr/bin/env fish
 
-if not fish_is_root_user
-	echo Run this script as root!
+if fish_is_root_user
+	echo Do not run this script as root!
 	exit
 end
 
 
-pacman -Syu --needed --noconfirm
+sudo pacman -Syu --needed --noconfirm
 
 echo Remove plymouth from /etc/mkinitcpio.conf
 echo Edit kernel cmdline in /etc/default/limine, add rcutree.enable_rcu_lazy=1, ipv6.disable=1
 read
 
-touch /etc/systemd/zram-generator.conf
+sudo touch /etc/systemd/zram-generator.conf
 
-pacman -Rsn plymouth cachyos-plymouth-theme --noconfirm
+sudo pacman -Rsn plymouth cachyos-plymouth-theme --noconfirm
 
-limine-mkinitcpio
+sudo limine-mkinitcpio
 
-pacman -S asusctl brightnessctl discord hypridle hyprland hyprlock hyprpaper imv lazygit libreoffice-fresh librewolf mpv ncspot nvim obs-studio proton-vpn-gtk-app rofi-wayland supergfxctl swww uwsm vpl-gpu-rt v4l2loopback-dkms xdg-desktop-portal-hyprland yazi --needed --noconfirm
+sudo pacman -S asusctl brightnessctl discord hypridle hyprland hyprlock hyprpaper imv lazygit libreoffice-fresh librewolf mpv ncspot nvim obs-studio proton-vpn-gtk-app rofi-wayland supergfxctl swww uwsm vpl-gpu-rt v4l2loopback-dkms xdg-desktop-portal-hyprland yazi --needed --noconfirm
 
 asusctl -c 80
 asusctl aura static -c b4befe
 asusctl aura-power keyboard -a
 
-systemctl enable --now supergfxd
-systemctl --user enable --now hypridle.service
+sudo systemctl enable --now supergfxd
+sudo systemctl --user enable --now hypridle.service
 
 powerprofilesctl configure-battery-aware --disable
 powerprofilesctl set power-saver
@@ -36,5 +36,3 @@ paru -Sa ags-hyprpanel-git hyprshot rose-pine-hyprcursor --needed --noconfirm
 echo Configure supergfxd at /etc/supergfxd.conf
 echo After reboot, configure discord, git config --global user.email/name, librewolf, obs-studio
 read
-
-systemctl reboot
