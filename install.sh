6cd2a39310053fffd26a329f8fb3ee36d59e805d
lewisrai <144852757+lewisrai@users.noreply.github.com>
2025-05-31 14:41:05 +0100

    Use bash instead of fish

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+
+packages=(
+    "asusctl"
+    "blueman"
+    "brightnessctl"
+    "discord"
+    "dunst"
+    "hypridle"
+    "hyprland"
+    "hyprlock"
+    "hyprpaper"
+    "hyprpolkitagent"
+    "imv"
+    "lazygit"
+    "libreoffice-fresh"
+    "librewolf"
+    "mpv"
+    "ncspot"
+    "nm-connection-editor"
+    "nvim"
+    "nwg-look"
+    "obs-studio"
+    "otf-firamono-nerd"
+    "proton-vpn-gtk-app"
+    "rofi-wayland"
+    "supergfxctl"
+    "uwsm"
+    "vpl-gpu-rt"
+    "v4l2loopback-dkms"
+    "waybar"
+    "xdg-desktop-portal-hyprland"
+    "yazi"
+)
+
+
+sudo pacman -Syu --needed --noconfirm
+
+sudo sed -i -e "s/plymouth //" /etc/mkinitcpio.conf
+sudo sed -i -e "s/quiet nowatchdog/quiet rcutree.enable_rcu_lazy=1 ipv6.disable=1 nowatchdog/" /etc/default/limine
+
+sudo touch /etc/systemd/zram-generator.conf
+
+sudo pacman -Rsn plymouth cachyos-plymouth-bootanimation --noconfirm
+
+sudo limine-mkinitcpio
+
+sudo pacman -S "${packages[@]}" --needed --noconfirm
+
+paru -Syua
+paru -Sa hyprshot rose-pine-hyprcursor --needed --noconfirm
+
+asusctl -c 80
+asusctl aura static -c b4befe
+asusctl aura-power keyboard -a
+
+powerprofilesctl configure-battery-aware --disable
+powerprofilesctl set power-saver
+
+sudo systemctl enable --now supergfxd
+sudo systemctl --user --global enable hypridle.service
+sudo systemctl --user --global enable hyprpolkitagent.service
+
+echo "Configure supergfxd at /etc/supergfxd.conf"
+echo "After reboot, configure discord, git config --global user.email/name, gtk, librewolf, obs-studio"
+read
