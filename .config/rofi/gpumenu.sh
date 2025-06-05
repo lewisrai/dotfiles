9a5a6abd3e944c79e3cc28a3f600bc7cb96a6970
lewisrai <144852757+lewisrai@users.noreply.github.com>
2025-06-05 15:33:44 +0100

    Gpu switch fix

--- .config/rofi/gpumenu.sh
+++ .config/rofi/gpumenu.sh
@@ -7,6 +7,6 @@ nvidia=' Nvidia'
 chosen="$(printf "$integrated\n$nvidia" | rofi -dmenu -theme ~/.config/rofi/menu.rasi)"
 
 case ${chosen} in
-    $integrated) pkexec sed -i -e 's/AsusMuxDgpu/Integrated/' /etc/supergfxd.conf && systemctl poweroff;;
-    $nvidia) pkexec sed -i -e 's/Integrated/AsusMuxDgpu/' /etc/supergfxd.conf && systemctl poweroff;;
+    $integrated) pkexec sed -i -e 's/AsusMuxDgpu/Integrated/' /etc/supergfxd.conf && systemctl restart supergfxd && systemctl poweroff;;
+    $nvidia) pkexec sed -i -e 's/Integrated/AsusMuxDgpu/' /etc/supergfxd.conf && systemctl restart supergfxd && systemctl poweroff;;
 esac
