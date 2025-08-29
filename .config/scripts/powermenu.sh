1d5559c1784248046a621e18fff234c219bc76ae
lewisrai <144852757+lewisrai@users.noreply.github.com>
2025-08-29 18:28:52 +0100

    Update xwayland script

--- .config/scripts/powermenu.sh
+++ .config/scripts/powermenu.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-
-
 lock=''
 suspend=''
 logout='󰍃'
--- .config/scripts/toggle-xwayland.sh
+++ .config/scripts/toggle-xwayland.sh
@@ -4,7 +4,7 @@ if cat ~/.config/hypr/hyprland.conf | grep 'xwayland:enabled = true'; then
 
     case $(echo -e "$no\n$yes" | rofi -dmenu -theme ~/.config/rofi/menu.rasi) in
         $no) ;;
-        $yes) sed -i -e 's|xwayland:enabled = true|xwayland:enabled = false|' ~/.config/hypr/hyprland.conf && hyprctl dispatch exit && hyprland;;
+        $yes) hyprctl dispatch exit && sed -i -e 's|xwayland:enabled = true|xwayland:enabled = false|' ~/.config/hypr/hyprland.conf;;
     esac
 else
     no='cancel'
@@ -12,6 +12,6 @@ else
 
     case $(echo -e "$no\n$yes" | rofi -dmenu -theme ~/.config/rofi/menu.rasi) in
         $no) ;;
-        $yes) sed -i -e 's|xwayland:enabled = false|xwayland:enabled = true|' ~/.config/hypr/hyprland.conf && hyprctl dispatch exit && hyprland;;
+        $yes) hyprctl dispatch exit && sed -i -e 's|xwayland:enabled = false|xwayland:enabled = true|' ~/.config/hypr/hyprland.conf;;
     esac
 fi
