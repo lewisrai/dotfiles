8fb3016a90b2653668379000774d49262871b660
lewisrai <144852757+lewisrai@users.noreply.github.com>
2025-09-05 18:16:57 +0100

    Restart on changing xwayland

--- .config/scripts/toggle-xwayland.sh
+++ .config/scripts/toggle-xwayland.sh
@@ -1,15 +1,22 @@
 no='cancel'
+pid=pidof hyprland
 
 if cat ~/.config/hypr/hyprland.conf | grep 'xwayland:enabled = true'; then
     yes='disable xwayland' 
 
     if [ "$(echo -e "$no\n$yes" | rofi -dmenu -theme ~/.config/rofi/menu.rasi)" == "$yes" ]; then
-        hyprctl dispatch exit && sed -i -e 's|xwayland:enabled = true|xwayland:enabled = false|' ~/.config/hypr/hyprland.conf;;
+        hyprctl dispatch exit
+        wait $pid
+        sed -i -e 's|xwayland:enabled = true|xwayland:enabled = false|' ~/.config/hypr/hyprland.conf
     fi
 else
     yes='enable xwayland' 
 
     if [ "$(echo -e "$no\n$yes" | rofi -dmenu -theme ~/.config/rofi/menu.rasi)" == "$yes" ]; then
-        hyprctl dispatch exit && sed -i -e 's|xwayland:enabled = false|xwayland:enabled = true|' ~/.config/hypr/hyprland.conf;;
+        hyprctl dispatch exit
+        wait $pid
+        sed -i -e 's|xwayland:enabled = false|xwayland:enabled = true|' ~/.config/hypr/hyprland.conf
     fi 
 fi
+
+hyprland
