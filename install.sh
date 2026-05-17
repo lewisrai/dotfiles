8f4a7c030c1a93da24adc02a3c25bbbed7bc4edd
lewisrai <144852757+lewisrai@users.noreply.github.com>
2026-05-17 13:04:35 +0100

    [fix] reorder steps

--- install.sh
+++ install.sh
@@ -59,19 +59,12 @@ packages=(
     'wbg'
 )
 
-mkdir ~/.icons/
+rm -rf ~/.*
 
-mv -f .config/* ~/.config/
-mv -f .icons/* ~/.icons/
-mv -f .local/share/* ~/.local/share/
+mv -f * ~/
 mv -f *. ~/
 
-rm -f .bash_logout
-
-sudo mkdir -p /usr/local/share/kbd/keymaps/
-sudo 7z x /usr/share/kbd/keymaps/i386/qwerty/uk.map.gz -o/usr/local/share/kbd/keymaps/
-sudo sed -i -e 's|Caps_Lock|Escape|' /usr/local/share/kbd/keymaps/uk.map
-sudo sed -i -e 's|uk|/usr/local/share/kbd/keymaps/uk.map|' /etc/vconsole.conf
+rm -rf ~/dotfiles/
 
 cat << 'EOF' | sudo tee -i /boot/limine.conf
 term_palette: 1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4
@@ -81,15 +74,22 @@ term_foreground: cdd6f4
 term_background_bright: 585b70
 term_foreground_bright: cdd6f4
 timeout: 0
+quiet: yes
 default_entry: 2
+
 EOF
 
-sudo sed -i -e 's|base ||' -e 's|udev|systemd|' -e 's|consolefont ||' -e 's|plymouth ||' /etc/mkinitcpio.conf
+sudo sed -i -e 's|plymouth ||' -e 's|sd-vconsole ||' /etc/mkinitcpio.conf
 sudo sed -i -e 's|"quiet|"vt.default_red=30,243,166,249,137,203,137,205,30,243,166,249,137,203,137,205 vt.default_grn=30,139,227,226,180,166,220,214,30,139,227,226,180,166,220,214 vt.default_blu=46,168,161,175,250,247,235,244,46,168,161,175,250,247,235,244 vt.cur_default=0x641517 i915.force_probe=!46a6 xe.force_probe=46a6 ipv6.disable=1 rcutree.enable_rcu_lazy=1 split_lock_detect=off systemd.zram=0 quiet|' -e 's|splash ||' /etc/default/limine
 
-sudo pacman -Rsn cachyos-plymouth-bootanimation pavucontrol plymouth switcheroo-control --noconfirm
+sudo pacman -Rsn cachyos-plymouth-bootanimation cachyos-plymouth-theme plymouth switcheroo-control --noconfirm
 
 paru -Syu "${packages[@]}"
+
+sudo mkdir -p /usr/local/share/kbd/keymaps/
+sudo 7z x /usr/share/kbd/keymaps/i386/qwerty/uk.map.gz -o/usr/local/share/kbd/keymaps/
+sudo sed -i -e 's|Caps_Lock|Escape|' /usr/local/share/kbd/keymaps/uk.map
+sudo sed -i -e 's|uk|/usr/local/share/kbd/keymaps/uk.map|' /etc/vconsole.conf
 
 asusctl battery limit 80
 asusctl aura effect static --colour f5c2e7
@@ -122,22 +122,20 @@ Description=set-powersave
 [Service]
 Type=oneshot
 RemainAfterExit=yes
-ExecStart=/bin/bash -c 'echo auto | tee /sys/bus/usb/devices/3-9/power/control /sys/bus/pci/devices/0000:00:0a.0/power/control /sys/bus/pci/devices/0000:00:00.0/power/control /sys/bus/pci/devices/0000:02:00.0/power/control /sys/bus/pci/devices/0000:03:00.0/power/control'
+ExecStart=/bin/bash -c 'echo auto | tee /sys/bus/usb/devices/3-9/power/control /sys/bus/pci/devices/0000:00:00.0/power/control /sys/bus/pci/devices/0000:00:0a.0/power/control /sys/bus/pci/devices/0000:02:00.0/power/control'
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+sudo systemctl enable --now no-turbo.service scx_loader.service set-powersave.service supergfxd.service
+
 sudo sed -i -e 's|#AutoEnable=true|AutoEnable=false|' /etc/bluetooth/main.conf
 sudo sed -i -e 's|Hybrid|Integrated|' -e 's|None|Asus|' -e 's|reboot": false|reboot": true|' /etc/supergfxd.conf
 
-sudo systemctl enable no-turbo.service scx_loader.service set-powersave.service supergfxd.service
-sudo systemctl --user --global enable hypridle.service
+sudo systemctl restart supergfxd.service
 
 sudo sbctl create-keys
-sudo sbctl enroll-keys
+sudo sbctl enroll-keys --microsoft
 sudo limine-enroll-config
 sudo limine-update
-j
-mv install.sh ~/
-rm -rf ~/dotfiles/
